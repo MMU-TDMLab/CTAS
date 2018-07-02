5a2130ac9b41c3eadd4bed841db809bc9ee98d0e
Andy <andrewpeliza@hotmail.com>
2018-07-02 15:32:03 +0100

    Token now stored locally with 1hr duration, logout button fixed.
    Next Authorisation: control which user can edit or delete which post etc.

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -8,6 +8,7 @@ import { Router } from '@angular/router';
 @Injectable({ providedIn: 'root'})
 export class AuthService {
   private token: string;
+  private tokenTimer: any;
   private isAuthenticated = false;
   private authStatus = new Subject<boolean>();
 
@@ -35,23 +36,75 @@ export class AuthService {
 
   userLogin(email: string, password: string, role: string) {
     const authData: AuthData = {email: email, password: password, role: role};
-    this.http.post<{token: string}>('http://localhost:3000/api/user/login', authData)
+    this.http.post<{token: string, expiresIn: number}>('http://localhost:3000/api/user/login', authData)
     .subscribe(response => {
       const token = response.token;
       this.token = token;
       if (token) {
+        const expiresDuration = response.expiresIn;
+        this.authTimer(expiresDuration);
         this.isAuthenticated = true;
         this.authStatus.next(true);
+        const now = new Date();
+        const expirationDate = new Date(now.getTime() + expiresDuration * 1000);
+        // console.log(expirationDate);
+        this.saveAuthData(token, expirationDate);
         this.router.navigate(['/']);
       }
     });
+  }
+
+  authUser() {
+    const authInfo = this.getAuthData();
+    if (!authInfo) {
+      return;
+    }
+    const now = new Date();
+    const expiresIn = authInfo.expirationDate.getTime() - now.getTime();
+    if (expiresIn > 0) {
+      this.token = authInfo.token;
+      this.isAuthenticated = true;
+      this.authTimer(expiresIn / 1000);
+      this.authStatus.next(true);
+    }
   }
 
   logout() {
     this.token = null;
     this.isAuthenticated = false;
     this.authStatus.next(false);
+    clearTimeout(this.tokenTimer);
+    this.clearAuthData();
     this.router.navigate(['/home']);
+  }
+
+  private saveAuthData(token: string, expirationDate: Date) {
+    localStorage.setItem('token', token);
+    localStorage.setItem('expiration', expirationDate.toISOString());
+  }
+
+  private clearAuthData() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expiration');
+  }
+
+  private getAuthData() {
+    const token = localStorage.getItem('token');
+    const expirationDate = localStorage.getItem('expiration');
+    if (!token || !expirationDate) {
+      return;
+    }
+    return {
+      token: token,
+      expirationDate: new Date(expirationDate)
+    };
+  }
+
+  private authTimer(duration: number) {
+    console.log('setting timer: ' + duration);
+    this.tokenTimer = setTimeout(() => {
+      this.logout();
+    }, duration * 1000);
   }
 
 }
