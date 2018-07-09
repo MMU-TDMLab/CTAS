202241099da89e716adab56021e1fc280ff86bd8
Andy <andrewpeliza@hotmail.com>
2018-07-09 11:31:38 +0100

    Setting annotations page, with scrollable div/ black outline & viewport

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -38,9 +38,12 @@ export class AuthService {
 
   createUser(email: string, password: string, role: string) {
     const authData: AuthData = {email: email, password: password, role: role};
-    this.http.post('http://localhost:3000/api/user/signup', authData)
+    return this.http
+    .post('http://localhost:3000/api/user/signup', authData)
       .subscribe(response => {
         console.log(response);
+      }, error => {
+        console.log(error);
       });
   }
 
