8ad2ef5b01157d1249b3e129ca4cf18513108ccb
Andy <andrewpeliza@hotmail.com>
2018-07-22 15:58:11 +0100

    Multer file middleware moved, cleaned code.

---
+++ backend/middleware/file.js
@@ -0,0 +1,45 @@
+const multer = require('multer');
+
+// const mimeTypeMap = {
+//   'file/.txt': 'text/plain',
+//   'file/txt': 'text/plain',
+//   'file/pdf': 'pdf',
+//   'file/text': 'txt',
+//   'file/txt': 'txt',
+//   'file/word': 'word'
+// }
+//////
+
+// const mimeTypeMap = multer({
+//   fileFilter: function (req, file, cb) {
+
+//     var filetypes = /txt|text/;
+//     var mimetype = filetypes.test(file.mimetype);
+//     var extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+
+//     if (mimetype && extname) {
+//       return cb(null, true);
+//     }
+//     cb("Error: File upload only supports the following filetypes - " + filetypes);
+//   }
+// });
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    // const isValid = mimeTypeMap[file.mimetype];
+    // let error = new Error('Invalid mime type');
+    // if (!isValid) {
+    //   error = null;
+    // }
+    cb(null, 'backend/documents');
+  },
+  filename: (req, file, cb) => {
+    const name = file.originalname.toLowerCase().split(' ').join('-');
+    const nameWithoutExtension = name.replace(/\.[^/.]+$/, "");
+    const fileExtention = file.originalname.split('.').pop();
+    const fileName = nameWithoutExtension;
+    cb(null, fileName + '-' + Date.now() + '.' + fileExtention);
+  }
+});
+
+module.exports = multer({ storage: storage }).single('file');
--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,61 +1,19 @@
 const express = require('express');
-const multer = require('multer');
+
 const router = express.Router();
 
 const authCheck = require('../middleware/check-auth');
 const PostController = require('../controllers/posts');
-
-
-// const mimeTypeMap = {
-//   'file/.txt': 'text/plain',
-//   'file/txt': 'text/plain',
-//   'file/pdf': 'pdf',
-//   'file/text': 'txt',
-//   'file/txt': 'txt',
-//   'file/word': 'word'
-// }
-//////
-
-// const mimeTypeMap = multer({
-//   fileFilter: function (req, file, cb) {
-
-//     var filetypes = /txt|text/;
-//     var mimetype = filetypes.test(file.mimetype);
-//     var extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-
-//     if (mimetype && extname) {
-//       return cb(null, true);
-//     }
-//     cb("Error: File upload only supports the following filetypes - " + filetypes);
-//   }
-// });
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    // const isValid = mimeTypeMap[file.mimetype];
-    // let error = new Error('Invalid mime type');
-    // if (!isValid) {
-    //   error = null;
-    // }
-    cb(null, 'backend/documents');
-  },
-  filename: (req, file, cb) => {
-    const name = file.originalname.toLowerCase().split(' ').join('-');
-    const nameWithoutExtension = name.replace(/\.[^/.]+$/, "");
-    const fileExtention = file.originalname.split('.').pop();
-    const fileName = nameWithoutExtension;
-    cb(null, fileName + '-' + Date.now() + '.' + fileExtention);
-  }
-});
+const extractFile = require('../middleware/file');
 
 router.post("",
   authCheck,
-  multer({ storage: storage }).single('file'),
+  extractFile,
   PostController.createPost);
 
 router.put("/:id",
   authCheck,
-  multer({ storage: storage }).single('file'),
+  extractFile,
   PostController.updatePost);
 
 router.get("", PostController.getPosts);
