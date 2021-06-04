f0bc9673cd8f396876708b765e6dda29778028d2
robflynnyh <16026255@stu.mmu.ac.uk>
2021-06-05 00:10:19 +0100

    highlighting fix

--- src/app/highlighter/txt2JSON.js
+++ src/app/highlighter/txt2JSON.js
@@ -7,7 +7,7 @@ export class highlightManager{
         this.obj = [];
         this.buildObj();
         this.occurs;
-        this.optional = '<a class="optional" style=text-decoration:none; color:blue">'
+        this.optional = '<a class="optional" style="text-decoration:none; color:blue">'
         this.clickable = '<a class="clickable" style="background-color: yellow; text-decoration: none; cursor:pointer;">'
     } //we should store highlighted words;
     buildObj(){
